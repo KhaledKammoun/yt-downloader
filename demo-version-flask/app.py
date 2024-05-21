9f222f9d2ee0ecb422750e93c972081b684f7073
KhaledKammoun <khaledsfax03@gmail.com>
2024-05-21 14:18:58 +0100

    YT - Downloader - V0.1

---
+++ demo-version-flask/app.py
@@ -0,0 +1,114 @@
+from flask import Flask, render_template, request
+from pytube import YouTube, Playlist
+import os
+import shutil
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
+
+app = Flask(__name__)
+
+def download_playlist(playlist_url, output_path='~/Downloads'):
+    try:
+        # Create a Playlist object
+        playlist = Playlist(playlist_url)
+        # Create a folder for the playlist
+        playlist_folder = output_path + '/' + playlist.title.replace(' ','_')
+        os.makedirs(playlist_folder, exist_ok=True)
+
+        # Print playlist details
+        print(f"Downloading playlist: {playlist.title}")
+
+        # Iterate through each video in the playlist and download it
+        i = 1
+        for video_url in playlist.video_urls:
+            print("Video {} :\n".format(i))
+            if (i >= 10) :
+                download_video(video_url, playlist_folder, str(i))
+            i += 1
+
+        print("Playlist Download complete!")
+
+    except Exception as e:
+        print(f"An error occurred while downloading playlist: {playlist.title}")
+        print(f"Error details: {e}")
+
+def concat_video_audio(output_path, video_audio_path, title):
+    video = VideoFileClip(video_audio_path + "/video.mp4")
+    audio = AudioFileClip(video_audio_path + "/audio.mp3")
+    final_clip = video.set_audio(audio)
+    output_file_path = output_path + "/" + title + ".mp4"
+    final_clip.write_videofile(output_file_path)
+
+
+def download_video(video_url, output_path='~/Downloads', name="1", target_resolution="1080p"):
+    try:
+        
+
+        # Create a YouTube object
+        yt = YouTube(video_url)
+        # video_stream = yt.streams.get_highest_resolution()
+        video_stream = None
+        print(yt.streams)
+        for stream in yt.streams:
+            if stream.resolution == target_resolution and stream.mime_type == "video/mp4":
+                video_stream = stream
+                break
+
+        if (video_stream == None) : 
+            # Get the highest resolution stream
+            stream = yt.streams.get_highest_resolution()
+            print("1080p quality not exist !!!")
+            
+            print(f"Downloading: {yt.title}")
+            print(f"Resolution: {stream.resolution}")
+
+            # Download the video to the current directory
+            stream.download(output_path, name)
+            print("Download complete!")
+        else :
+            print(video_stream)
+            print("111")            
+            new_folder = "/New_Folder"
+            # create a new  folder
+            video_path = output_path + new_folder
+            os.makedirs(video_path, exist_ok=True)
+
+            audio_stream = yt.streams.get_audio_only()
+            if video_stream and audio_stream:
+                # Print video details
+                print(f"Downloading: {yt.title}")
+                print(f"Resolution: {video_stream.resolution}")
+
+                # Download the video
+                video_stream.download(video_path, "video.mp4")
+                # Download the audio
+                audio_stream.download(video_path, "audio.mp3")
+
+                # Concat video and audio
+                concat_video_audio(output_path, video_path, name)
+                print("Download complete!")
+            else:
+                print(f"No video stream available for {yt.title}")
+
+        # delete the new folder
+        shutil.rmtree(video_path)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        output_path = request.form['output_path']
+        desition = int(request.form['desition'])
+        if desition == 1:
+            playlist_url = request.form['playlist_url']
+            download_playlist(playlist_url, output_path)
+        elif desition == 2:
+            video_url = request.form['video_url']
+            target_resolution = "480p"
+            download_video(video_url, output_path, target_resolution)
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,177 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-export default function Home() {
+const MainPage = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+    setIsMobile(isMobile);
+  };
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      const screenWidth = window.innerWidth;
+      setIsMobile(screenWidth <= 768); // Change 768 to your desired breakpoint
+    };
+
+    checkScreenWidth();
+
+    window.addEventListener("resize", checkScreenWidth);
+
+    return () => {
+      window.removeEventListener("resize", checkScreenWidth);
+    };
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      {/* Navigation Bar */}
+      <nav className=" from-black via-gray-800 to-blue-200 p-4 flex items-center justify-between w-full">
+        {/* Logo or Brand */}
+        <div className="text-lg font-bold hover:cursor-pointer">
+          <span className="bg-gradient-to-r from-red-500 to-red-700 text-white px-2 py-1 rounded">
+            YT
+          </span>{" "}
+          Downloader
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        {/* Navigation Links - Desktop */}
+        <div className="hidden  items-center justify-between md:flex space-x-4 text-gray-600 dark:text-gray-300">
+          <a href="#" className="block md:px-4 transition hover:text-red-500">
+            Home
+          </a>
+          <a href="#" className="block md:px-4 transition hover:text-red-500">
+            About
+          </a>
+          <a href="#" className="block md:px-4 transition hover:text-red-500">
+            Services
+          </a>
+          <Link
+            href="/Auth"
+            className="block bg-gradient-to-r from-red-500 to-red-700 text-white px-2 py-1 rounded text-center font-semibold transition duration-300 hover:scale-105 active:scale-95"
+          >
+            <span className="relative text-sm font-semibold text-white">
+              Get Started
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          </Link>
+          {/* Add more links as needed */}
+        </div>
+        {/* Navigation Button - Mobile */}
+        <button
+          className="md:hidden focus:outline-none"
+          onClick={toggleNav}
+          aria-label="Toggle Navigation"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            {isNavOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            )}
+          </svg>
+        </button>
+      </nav>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      {/* Mobile Navigation */}
+      {isNavOpen && isMobile && (
+        <div className="absolute top-14 right-2 w-36 bg-gray-800 shadow-md p-2 rounded-lg">
+          <a href="#" className="block mb-2 text-center hover:text-red-500">
+            Home
+          </a>
+          <a href="#" className="block mb-2 text-center hover:text-red-500">
+            About
+          </a>
+          <a href="#" className="block mb-2 text-center hover:text-red-500">
+            Services
+          </a>
+          <Link
+            href="/Auth"
+            className="block bg-gradient-to-r from-red-500 to-red-700 text-white px-2 py-1 rounded text-center font-semibold transition duration-300 hover:scale-105 active:scale-95"
+          >
+            Get Started
+          </Link>
+          {/* Add more links as needed */}
+        </div>
+      )}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {/* Main Content */}
+      <main className="flex flex-col items-center justify-center flex-1">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-8">
+          Save Videos for Later{" "}
+          <span className="text-gradient">Enjoyment.</span>
+        </h1>
+        <h1 className="text-4xl font-bold">
+          <span className="bg-gradient-to-r from-red-500 to-red-700 text-white px-2 py-1 rounded">
+            YT
+          </span>{" "}
+          Downloader
+        </h1>
+        {/* Additional content or sections can be added here */}
+      </main>
+
+      {/* Footer */}
+
+      <footer
+        className={`bg-white ${
+          isMobile ? "flex items-center justify-center" : ""
+        } rounded-lg shadow m-2 dark:bg-gray-800`}
+      >
+        <div className=" mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            &copy; {new Date().getFullYear()}{" "}
+            <a
+              href="https://github.com/KhaledKammoun"
+              className="text-red-400 hover:text-red-500"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Khaled Kammoun
+            </a>
+            . All rights reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
   );
-}
+};
+
+export default MainPage;
